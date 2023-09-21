6c90eee8d79a4e9fae71c1f1be02183fc97c1f7a
binh <>
2023-09-21 16:32:03 +0700

    update

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema(
 
     {
         username: {type: String, require: true, unique: true},
+        phone: {type: String, require: true, unique: false},
         email: {type: String, require: true, unique : true},
         password: {type : String , require: true, unique: true},
         location: {type: String, require: false},
